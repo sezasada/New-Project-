04432c355382688e5cec10a16dfb49fe1e132c14
sezasada <111367099+sezasada@users.noreply.github.com>
2022-12-14 11:30:46 -0800

    js and html
    fixed some stuff.

--- newproject.js
+++ newproject.js
@@ -4,19 +4,26 @@ let numbers = [];
 // empty array that will contain values being added from the interface.
 
 function addNumbers(numberOne, numberTwo){
-    console.log('Adding numbers:', numberOne, numberTwo);
+    console.log('Adding numbers:', numberOne, 'and', numberTwo);
     const numberObject = {
         valueOne: numberOne,
         valueTwo: numberTwo,
+        answer: numberOne + numberTwo
     }
+    console.log(numberOne + numberTwo);
     numbers.push(numberObject);
-    return numberOne + numberTwo;
 }
-console.log(numbers);
-console.log('Answer is', addNumbers(2, 4));
-console.log(numbers);
+console.log(addNumbers(2, 4));
+
 
 function main(){
-    console.log('in main');
-    
+    $( '#resultButton' ).on( 'click', sayNumbers );
+}   
+
+function sayNumbers(){
+    let a = parseInt(prompt( 'Enter First Number' ));
+    let b = parseInt(prompt( 'Enter Second Number' ));
+    console.log(numbers.push(addNumbers(a, b)));
+    console.log('Answer is:', a += b);
 }
+console.log(numbers);
